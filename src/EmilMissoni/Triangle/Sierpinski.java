40be73a693e07e8b3d8f1c91273335d3c278c9d5
Emil Missoni <pi.zero.w.em@gmail.com>
2022-01-16 09:12:40 +0100

    small update

--- src/EmilMissoni/Triangle/Sierpinski.java
+++ src/EmilMissoni/Triangle/Sierpinski.java
@@ -14,7 +14,7 @@ public class Sierpinski {
         Random random = new Random();
         random.setSeed(seed);
         RGB start_color = new RGB(random.nextInt(0, 120), random.nextInt(0, 120), random.nextInt(0, 120));
-        RGB end_color = new RGB(random.nextInt(80, 200), random.nextInt(80, 180), random.nextInt(80, 170));
+        RGB end_color = new RGB(random.nextInt(80, 140), random.nextInt(80, 140), random.nextInt(80, 200));
         SVG svg = new SVG(filename, 540, 720);
         SierpinskiTriangle sierpinskiTriangle = new SierpinskiTriangle(start_color, end_color, pos, 250);
         sierpinskiTriangle.generate(svg, 6, pos, 250, 3);
